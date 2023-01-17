0709eb3a64cd952b847380ba62c73a0278187dfc
Dana Truong <tramtramphotos@gmail.com>
2023-01-17 13:01:02 -0600

    Clean up code

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,14 +2,3 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
     .then(response => response.json())
 }
-
-export const postNewOrder = (newOrder) => {
-  fetch('http://localhost:3001/api/v1/orders', {
-    method: 'POST',
-    body: JSON.stringify(newOrder),
-    headers: { "Content-Type": "application/json" }
-  })
-    .then(response => response.json())
-    .then(data => this.setState({ orders: [...this.state.orders, data] }))
-    .catch(error => console.log('post error:', error))
-}
